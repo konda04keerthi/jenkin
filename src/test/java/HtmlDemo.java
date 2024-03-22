1c6860ec3868818dd523930d03d6bf86ab79c2e1
konda04keerthi <164127703+konda04keerthi@users.noreply.github.com>
2024-03-22 12:47:17 +0530

    Update HtmlDemo.java

--- src/test/java/HtmlDemo.java
+++ src/test/java/HtmlDemo.java
@@ -8,6 +8,7 @@ public class HtmlDemo {
 		driver.get("https://www.amazon.in");
 		System.out.println("Title is : "+driver.getTitle());
 		System.out.println("hi");
+		System.out.println("hii");
 
 	}
 
